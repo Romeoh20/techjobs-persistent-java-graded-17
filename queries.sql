9290075ea051cd9fffae15a5f8f84671a34bb27e
romeo <romeoquist@gmail.com>
2023-11-13 13:40:35 -0600

    Finished The Project

--- queries.sql
+++ queries.sql
@@ -1,7 +1,21 @@
 --Part 1
 
+--id: int
+--employer: varchar
+--name: varchar
+--skills: varchar
+
 --Part 2
+
+SELECT name FROM employer WHERE location = "St. Louis City";
 
 --Part 3
 
+DROP TABLE job;
+
 --Part 4
+
+SELECT * FROM skill
+LEFT JOIN job_skills ON skill_id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
